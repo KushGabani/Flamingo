89803b0c05058cd1a79dc69ea08d6ee6c5fca7e1
KushGabani <kushgabz2687@gmail.com>
2021-05-19 17:57:50 +0530

    Finished UI for Music Generation module

--- app.py
+++ app.py
@@ -12,5 +12,20 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/about", methods=['GET'])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/music-generation", methods=['GET'])
+def music_generation():
+    return render_template("music_generation.html")
+
+
+@app.route("/style-transfer", methods=['GET'])
+def style_transfer():
+    return render_template("style_transfer.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
